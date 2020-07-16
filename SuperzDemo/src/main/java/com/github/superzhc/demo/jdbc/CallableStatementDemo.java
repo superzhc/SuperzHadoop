d064fd7fbe979fa3103f4f7187a9c98861d0c83f
superzhc <zhengchao0555@163.com>
2020-07-16 21:25:15 +0800

    jdbc示例

---
+++ SuperzDemo/src/main/java/com/github/superzhc/demo/jdbc/CallableStatementDemo.java
@@ -0,0 +1,10 @@
+package com.github.superzhc.demo.jdbc;
+
+/**
+ * 当想要访问数据库存储过程时使用。CallableStatement接口也可以接受运行时输入参数
+ * 2020年07月16日 superz add
+ */
+public class CallableStatementDemo
+{
+    // TODO
+}
---
+++ SuperzDemo/src/main/java/com/github/superzhc/demo/jdbc/DBCloseUtils.java
@@ -0,0 +1,49 @@
+package com.github.superzhc.demo.jdbc;
+
+import java.sql.*;
+
+/**
+ * 2020年07月16日 superz add
+ */
+public class DBCloseUtils
+{
+    public static void close(Connection connection) {
+        try {
+            if (null != connection)
+                connection.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(Statement stmt) {
+        try {
+            if (null != stmt)
+                stmt.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(PreparedStatement pstmt) {
+        try {
+            if (null != pstmt)
+                pstmt.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void close(ResultSet rs) {
+        try {
+            if (null != rs)
+                rs.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
