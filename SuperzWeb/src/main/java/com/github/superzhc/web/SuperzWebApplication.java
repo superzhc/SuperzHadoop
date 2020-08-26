b58db384074ffd65ec5f638b5e0671471aa528df
superzhc <zhengchao0555@163.com>
2020-08-26 23:47:14 +0800

    web project init

---
+++ SuperzWeb/src/main/java/com/github/superzhc/web/SuperzWebApplication.java
@@ -0,0 +1,14 @@
+package com.github.superzhc.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperzWebApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperzWebApplication.class, args);
+    }
+
+}
