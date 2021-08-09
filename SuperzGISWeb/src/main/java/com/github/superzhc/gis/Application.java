064143d3d07ab2689af98bfdc752ea8740f6b168
superzhc <zhengchao0555@163.com>
2021-08-09 22:03:30 +0800

    地图接口测试

---
+++ SuperzGISWeb/src/main/java/com/github/superzhc/gis/Application.java
@@ -0,0 +1,15 @@
+package com.github.superzhc.gis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author superz
+ * @create 2021/8/9 17:57
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
