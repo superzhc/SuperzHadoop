94ffc8f5285bce7475e88ca0ec75bda34fa27da4
superzhc <zhengchao0555@163.com>
2021-08-24 19:36:11 +0800

    2021年8月24日 GIS简单查询工具

---
+++ SuperzGISWeb/src/main/java/com/github/superzhc/gis/web/dto/GeomesaToolDTO.java
@@ -0,0 +1,20 @@
+package com.github.superzhc.gis.web.dto;
+
+import lombok.Data;
+
+/**
+ * @author superz
+ * @create 2021/8/23 16:57
+ */
+@Data
+public class GeomesaToolDTO {
+    private String hbaseZookeepers;
+    private String hbaseCoprocessorUrl;
+    private String hbaseCatalog;
+
+    private String schema;
+    private String ecql;
+    private Integer number;
+    private String sortField;
+    private String sortOrder;
+}
