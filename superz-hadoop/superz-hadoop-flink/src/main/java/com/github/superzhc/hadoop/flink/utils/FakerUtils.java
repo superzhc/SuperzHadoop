d15965055978fce3ef21e03f28f468fd9da2d08d
superzhc <zhengchao0555@163.com>
2021-11-30 19:57:47 +0800

    java faker source update

--- superz-hadoop/superz-hadoop-flink/src/main/java/com/github/superzhc/hadoop/flink/utils/FakerUtils.java
+++ superz-hadoop/superz-hadoop-flink/src/main/java/com/github/superzhc/hadoop/flink/utils/FakerUtils.java
@@ -10,6 +10,7 @@ import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Random;
 
 /**
  * @author superz
@@ -65,6 +66,51 @@ public class FakerUtils {
 
     public static class Expression {
         private static final String EXPRESSION_TEMPLATE = "#{%s %s}";
+
+        public static final String NAME=expression("Name.name");
+        public static final String ID_CARD=regexify("(\\d{6})(\\d{4})(\\d{2})(\\d{2})(\\d{3})([0-9]|X)");
+        public static final String QQ = regexify("[1-9][0-9]{4,10}");
+        public static final String IP = regexify("((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)");
+
+        /**
+         * 新增车的正则表达式
+         */
+        public static class Car {
+            private static final String ORDINARY_LICENSE_PLATE_PATTERN = "[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}";
+            private static final String NEW_ENERGY_LICENSE_PLATE_PATTERN = "[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(([0-9]{5}[DF])|([DF][A-HJ-NP-Z0-9][0-9]{4}))";
+            private static final String LICENSE_PLATE_PATTERN = "([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(([0-9]{5}[DF])|(DF[0-9]{4})))|([京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳]{1})";
+            private static final String[] LICENSE_PLATE_COLOR_PATTERN = {"蓝", "黄", "白", "绿", "黑"};
+            /* 车架 VIN 正则表达式 */
+            private static final String VIN_PATTERN = "[A-HJ-NPR-Z\\d]{8}[X\\d][A-HJ-NPR-Z\\d]{3}\\d{5}";
+
+            /**
+             * 车牌号（包含新能源车牌号）
+             */
+            public static final String LICESE_PLATE = regexify(LICENSE_PLATE_PATTERN);
+
+            /**
+             * 普通车牌号
+             */
+            public static final String ORDINARY_LICENSE_PLATE = regexify(ORDINARY_LICENSE_PLATE_PATTERN);
+
+            /**
+             * 新能源车牌号
+             */
+            public static final String NEW_ENERGY_LICENSE_PLATE = regexify(NEW_ENERGY_LICENSE_PLATE_PATTERN);
+
+            /**
+             * 车辆的 vin
+             * <p>
+             * 规则：
+             * 1. 由大写字母和数字组成，长度17位；
+             * 2. 字母不会出现O、Q、I三个字母；
+             * 3. 第9位只能是【0-9】的数字和字母X;
+             * 4. 第13-17位只能是数字；
+             *
+             * @return
+             */
+            public static final String VIN = regexify(VIN_PATTERN);
+        }
 
         public static String name() {
             return expression("Name.name");
