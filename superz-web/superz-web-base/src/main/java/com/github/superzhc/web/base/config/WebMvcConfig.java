8b84a9f82df2c5246326a9fe85cce7aa2ea7867b
superzhc <zhengchao0555@163.com>
2021-12-07 17:04:06 +0800

    web module add

---
+++ superz-web/superz-web-base/src/main/java/com/github/superzhc/web/base/config/WebMvcConfig.java
@@ -0,0 +1,28 @@
+package com.github.superzhc.web.base.config;
+
+import com.github.superzhc.web.base.interceptor.LoggerInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author superz
+ * @create 2021/12/7 13:43
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggerInterceptor loggerInterceptor;
+
+    /**
+     * 注册拦截器
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggerInterceptor).addPathPatterns("/plc/infos");
+    }
+}
