d08ef225927aebbaec3c204f6aedd2bc7602c381
superzhc <zhengchao0555@163.com>
2022-09-19 18:12:59 +0800

    update

--- superz-common/superz-common-core/src/main/java/com/github/superzhc/common/utils/MapUtils.java
+++ superz-common/superz-common-core/src/main/java/com/github/superzhc/common/utils/MapUtils.java
@@ -3,8 +3,7 @@ package com.github.superzhc.common.utils;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 2020年11月23日 superz add
@@ -74,5 +73,104 @@ public class MapUtils {
         map.put(newKey, map.get(key));
         map.remove(key);
         return map;
+    }
+
+    public static <T> String print(Map<String, T> map) {
+        List<Map<String, T>> maps = new ArrayList<>();
+        maps.add(map);
+        return print(maps);
+    }
+
+    public static <T> String print(List<Map<String, T>> maps) {
+        // 获取所有的key
+        Set<String> keys = new LinkedHashSet<>();
+        for (Map<String, ?> map : maps) {
+            keys.addAll(map.keySet());
+        }
+
+        int[] columnMaxLengths = new int[keys.size()];
+        String[] headerRow = new String[keys.size()];
+        int i = 0;
+        for (String key : keys) {
+            columnMaxLengths[i] = length(key);
+            headerRow[i] = key;
+            i++;
+        }
+
+        List<String[]> rows = new ArrayList<>();
+        for (Map<String, ?> map : maps) {
+            String[] row = new String[keys.size()];
+            int j = 0;
+            for (String key : keys) {
+                String value = !map.containsKey(key) || null == map.get(key) ? null : String.valueOf(map.get(key));
+                row[j] = value;
+                columnMaxLengths[j] = Math.max(columnMaxLengths[j], (null == value ? 0 : length(value)));
+                j++;
+            }
+            rows.add(row);
+        }
+
+        StringBuilder result = new StringBuilder();
+        result.append(printSeparator(columnMaxLengths)).append("\n");
+        result.append(printRow(headerRow, columnMaxLengths)).append("\n");
+        result.append(printSeparator(columnMaxLengths)).append("\n");
+        for (String[] row : rows) {
+            result.append(printRow(row, columnMaxLengths)).append("\n");
+        }
+        result.append(printSeparator(columnMaxLengths)).append("\n");
+
+        return result.toString();
+    }
+
+    private static String printRow(String[] row, int[] columnMaxLengths) {
+        StringBuilder sb = new StringBuilder();
+        int columnCount = row.length;
+        for (int i = 0; i < columnCount; i++) {
+            sb.append("|");
+            sb.append(rightPad(row[i], columnMaxLengths[i]));
+        }
+        sb.append("|");
+        return sb.toString();
+    }
+
+    private static String printSeparator(int[] columnMaxLengths) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < columnMaxLengths.length; i++) {
+            sb.append("+");
+            for (int j = 0; j < columnMaxLengths[i] + 1; j++) {
+                sb.append("-");
+            }
+        }
+        sb.append("+");
+        return sb.toString();
+    }
+
+    private static String rightPad(String str, int maxLength) {
+        int len = length(str);
+        for (int i = 0; i < ((maxLength - len) + 1); i++) {
+            str += " ";
+        }
+        return str;
+    }
+
+    private static int length(String str) {
+        int valueLength = 0;
+        // String chinese = "[\u0391-\uFFE5]";//匹配中文字符的正则表达式： [\u4e00-\u9fa5]
+        // String doubleChar = "[^\\x00-\\xff]";// 匹配双字节字符(包括汉字在内)：[^\x00-\xff]
+        String doubleChar = "[\u1100-\u115F\u2E80-\uA4CF\uAC00-\uD7A3\uF900-\uFAFF\uFE10-\uFE19\uFE30-\uFE6F\uFF00-\uFF60\uFFE0-\uFFE6]";
+        /* 获取字段值的长度，如果含中文字符，则每个中文字符长度为2，否则为1 */
+        for (int i = 0; i < str.length(); i++) {
+            /* 获取一个字符 */
+            String temp = str.substring(i, i + 1);
+            /* 判断是否为中文字符 */
+            if (temp.matches(doubleChar)) {
+                /* 中文字符长度为2 */
+                valueLength += 2;
+            } else {
+                /* 其他字符长度为1 */
+                valueLength += 1;
+            }
+        }
+        return valueLength;
     }
 }
