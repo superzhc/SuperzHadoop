04353e55859d20d85ba75b54e0c4ce5d01dbf0be
superzhc <zhengchao0555@163.com>
2022-12-07 15:19:00 +0800

    hudi module

---
+++ superz-hadoop/superz-hadoop-hudi/src/main/java/com/github/superzhc/hadoop/hudi/HudiMain.java
@@ -0,0 +1,11 @@
+package com.github.superzhc.hadoop.hudi;
+
+/**
+ * @author superz
+ * @create 2022/12/7 15:18
+ **/
+public class HudiMain {
+    public static void main(String[] args) {
+
+    }
+}
