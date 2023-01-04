5b4d3a79e4d80b32e06495750f0a82d4b8e8f24c
superzhc <zhengchao0555@163.com>
2023-01-05 01:55:45 +0800

    mail tool update

--- superz-common/superz-common-core/src/main/java/com/github/superzhc/common/email/MailHelper.java
+++ superz-common/superz-common-core/src/main/java/com/github/superzhc/common/email/MailHelper.java
@@ -3,14 +3,14 @@ package com.github.superzhc.common.email;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.mail.Authenticator;
-import javax.mail.MessagingException;
-import javax.mail.PasswordAuthentication;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeUtility;
-import java.io.UnsupportedEncodingException;
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.io.File;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 
 /**
@@ -20,21 +20,76 @@ import java.util.Properties;
 public class MailHelper {
     private static final Logger LOG = LoggerFactory.getLogger(MailHelper.class);
 
-    private Session session;
-    private MimeMessage mimeMessage;
+    private String email = null;
+    private String password = null;
+    private Session session = null;
+    private MimeMessage message = null;
+
+    private MimeMultipart content = null;
 
     public MailHelper(Properties properties) {
         this.session = Session.getInstance(properties);
-        this.mimeMessage = new MimeMessage(this.session);
     }
 
-//    public MailHelper(Properties properties, String username, String password) {
-//        this.session = Session.getInstance(properties, new Authenticator() {
-//            protected PasswordAuthentication getPasswordAuthentication() {
-//                return new PasswordAuthentication(username, password);
-//            }
-//        });
-//    }
+    public MailHelper(Properties properties, String email, String password) {
+        this.email = email;
+        this.password = password;
+        this.session = Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(email, password);
+            }
+        });
+    }
+
+    public static MailHelper SMTP_ENT_QQ(String email, String password) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.host", "smtp.exmail.qq.com");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.from", email);
+        return new MailHelper(props, email, password);
+    }
+
+    public static MailHelper qq(String email, String password) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.host", "smtp.qq.com");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.from", email);
+        return new MailHelper(props, email, password);
+    }
+
+    public static MailHelper _163(String email, String password) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.ssl.enable", "true");
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.timeout", "10000");
+        props.put("mail.smtp.host", "smtp.163.com");
+        props.put("mail.smtp.port", "465");
+        props.put("mail.from", email);
+        return new MailHelper(props, email, password);
+    }
+
+    public MimeMessage getMessage() {
+        if (null == message) {
+            message = new MimeMessage(this.session);
+        }
+        return message;
+    }
+
+    private MimeMultipart getContent() {
+        if (null == content) {
+            content = new MimeMultipart("mixed");
+        }
+        return content;
+    }
 
     public MailHelper subject(String str) {
         return subject(str, "UTF-8");
@@ -42,17 +97,21 @@ public class MailHelper {
 
     public MailHelper subject(String str, String charset) {
         try {
-            mimeMessage.setSubject(str, charset);
+            getMessage().setSubject(str, charset);
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
         return this;
     }
 
-    public MailHelper from(String username, String nickname) {
+    public MailHelper from(String nickname) {
+        return from(this.email, nickname);
+    }
+
+    public MailHelper from(String email, String nickname) {
         try {
             String encodeNickname = MimeUtility.encodeText(nickname);
-            mimeMessage.setFrom(new InternetAddress(String.format("%s<%s>", encodeNickname, username)));
+            getMessage().setFrom(new InternetAddress(String.format("%s <%s>", encodeNickname, email)));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -69,12 +128,131 @@ public class MailHelper {
 
     public MailHelper replyTo(String replies) {
         try {
-            mimeMessage.setReplyTo(InternetAddress.parse(replies.replace(";", ",")));
+            getMessage().setReplyTo(InternetAddress.parse(replies.replace(";", ",")));
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
         return this;
     }
 
-//    public MailHelper
+    public MailHelper to(String... recipientArr) {
+        if (null == recipientArr || recipientArr.length == 0) {
+            throw new RuntimeException("At least one recipient");
+        }
+        return to(String.join(",", recipientArr));
+    }
+
+    public MailHelper to(String recipients) {
+        try {
+            getMessage().setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipients.replace(";", ",")));
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    public MailHelper cc(String... recipientArr) {
+        if (null == recipientArr || recipientArr.length == 0) {
+            throw new RuntimeException("At least one recipient");
+        }
+        return cc(String.join(",", recipientArr));
+    }
+
+    public MailHelper cc(String recipients) {
+        try {
+            getMessage().setRecipients(Message.RecipientType.CC, InternetAddress.parse(recipients.replace(";", ",")));
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    public MailHelper bcc(String... recipientArr) {
+        if (null == recipientArr || recipientArr.length == 0) {
+            throw new RuntimeException("At least one recipient");
+        }
+        return bcc(String.join(",", recipientArr));
+    }
+
+    public MailHelper bcc(String recipients) {
+        try {
+            getMessage().setRecipients(Message.RecipientType.BCC, InternetAddress.parse(recipients.replace(";", ",")));
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    public MailHelper text(String textStr) {
+        try {
+            MimeBodyPart bodyPart = new MimeBodyPart();
+            bodyPart.setText(textStr, "UTF-8");
+            getContent().addBodyPart(bodyPart);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    public MailHelper html(String htmlStr) {
+        try {
+            MimeBodyPart bodyPart = new MimeBodyPart();
+            bodyPart.setContent(htmlStr, "text/html; charset=utf-8");
+            getContent().addBodyPart(bodyPart);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    public MailHelper attach(File file) {
+        return attach(file, null);
+    }
+
+    public MailHelper attach(File file, String fileName) {
+        try {
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            FileDataSource fds = new FileDataSource(file);
+            attachmentPart.setDataHandler(new DataHandler(fds));
+            attachmentPart.setFileName(null == fileName ? MimeUtility.encodeText(fds.getName()) : MimeUtility.encodeText(fileName));
+            getContent().addBodyPart(attachmentPart);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    public MailHelper attach(URL url, String fileName) {
+        try {
+            MimeBodyPart attachmentPart = new MimeBodyPart();
+            DataHandler dataHandler = new DataHandler(url);
+            attachmentPart.setDataHandler(dataHandler);
+            attachmentPart.setFileName(null == fileName ? MimeUtility.encodeText(new SimpleDateFormat("yyyyMMddHHmmss").format(new Date())) : MimeUtility.encodeText(fileName));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return this;
+    }
+
+    public void send() {
+        try {
+            MimeMessage message = getMessage();
+            message.setContent(getContent());
+            message.setSentDate(new Date());
+            Transport.send(message);
+            LOG.info("发送邮件成功");
+        } catch (Exception e) {
+            throw new RuntimeException("发送邮件异常", e);
+        }
+    }
+
+    public static void main(String[] args) {
+        MailHelper mailHelper = MailHelper.qq("xxxx@qq.com", "your password");
+        mailHelper.from("易")
+                .to("zhengchao0555@163.com")
+                .subject("测试邮件")
+                .html("<h3>HTML部分</h3>")
+                .text("TEXT部分")
+                .send();
+    }
 }
--- superz-data/superz-data-core/src/main/java/com/github/superzhc/data/DataMain.java
+++ superz-data/superz-data-core/src/main/java/com/github/superzhc/data/DataMain.java
@@ -1,11 +1,13 @@
 package com.github.superzhc.data;
 
+import com.github.superzhc.common.email.MailHelper;
 import com.github.superzhc.common.jdbc.JdbcHelper;
 import com.github.superzhc.common.utils.MapUtils;
 import com.github.superzhc.data.fund.DoctorXiong;
 import com.github.superzhc.data.fund.EastMoneyFund;
 import com.github.superzhc.data.fund.SinaFund;
 import com.github.superzhc.data.news.MoFish;
+import com.github.superzhc.data.shopping.GuangDiu;
 import com.github.superzhc.data.stock.NetEaseStock;
 
 import java.util.LinkedHashMap;
@@ -27,7 +29,7 @@ public class DataMain {
         // String sql="create table sina_funds(id bigint auto_increment primary key,code varchar(8) not null,name varchar(255) not null,company_name varchar(255),subject_name varchar(255),create_date varchar(50),scale double,cxpj int,htpj int,jajxpj int,zspj int,yhpj3 int,yhpj5 int)";
 
 
-        try (JdbcHelper jdbc = new JdbcHelper(url, username, password)) {
+        // try (JdbcHelper jdbc = new JdbcHelper(url, username, password)) {
             // String sql = "insert into em_funds (code,name,type,pinyin,full_pinyin) values (?,?,?,?,?)";
             // List<Map<String, Object>> data = EastMoneyFund.funds();
             // jdbc.batchUpdate(sql, MapUtils.values(data, "code", "name", "type", "pinyin", "full_pinyin"));
@@ -36,6 +38,16 @@ public class DataMain {
             // List<Map<String, Object>> data = SinaFund.funds();
             // jdbc.batchUpdate(sql, MapUtils.values(data, "code", "name", "company_name", "subject_name", "create_date", "scale", "cxpj", "htpj", "jajxpj", "zspj", "yhpj3", "yhpj5"));
 
-        }
+        System.out.println(MapUtils.print(GuangDiu.all()));
+
+        MailHelper mailHelper = MailHelper.qq("1013849463@qq.com", "vvtvtlpssnkfbfjd");
+        mailHelper.from("易")
+                .to("zhengchao0555@163.com")
+                .subject("测试邮件")
+                .html("<h3>HTML部分</h3>")
+                .text("TEXT部分")
+                .send();
+
+        // }
     }
 }
