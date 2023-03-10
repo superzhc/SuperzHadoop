fd054250b5bc454c5ef0ef86080488a3599cd321
superzhc <zhengchao0555@163.com>
2023-03-10 10:43:09 +0800

    aktools add locale config

--- superz-data/superz-data-core/src/main/java/com/github/superzhc/data/other/AKTools.java
+++ superz-data/superz-data-core/src/main/java/com/github/superzhc/data/other/AKTools.java
@@ -6,9 +6,9 @@ import com.github.superzhc.common.jackson.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
 
 /**
  * @author superz
@@ -16,6 +16,35 @@ import java.util.Map;
  */
 public class AKTools {
     private static final Logger LOG = LoggerFactory.getLogger(AKTools.class);
+
+    // private static Properties localeProperties = new Properties();
+    private static Map<String, String> ch2enMap = new HashMap<>();
+
+    static {
+        synchronized (AKTools.class) {
+            try {
+                Properties localeProperties = new Properties();
+                InputStream stream = AKTools.class.getClassLoader().getResourceAsStream("akshare.properties");
+                localeProperties.load(stream);
+
+                for (String key : localeProperties.stringPropertyNames()) {
+                    String value = localeProperties.getProperty(key);
+                    String[] arr = value.split(",");
+                    for (String item : arr) {
+                        if (null == item || item.trim().length() == 0) {
+                            continue;
+                        }
+
+                        ch2enMap.put(item.trim(), key);
+                    }
+                }
+                LOG.info("加载资源【{}】成功","akshare.properties");
+            } catch (IOException e) {
+                LOG.error("加载资源【{}】失败！", "akshare.properties", e);
+                // ignore
+            }
+        }
+    }
 
     private static final Integer DEFAULT_PORT = 8080;
 
@@ -42,6 +71,26 @@ public class AKTools {
         String result = HttpRequest.get(url, params).body();
         JsonNode json = JsonUtils.loads(result);
         Map<String, Object>[] maps = JsonUtils.newObjectArray(json);
-        return Arrays.asList(maps);
+
+        if (null == maps || maps.length == 0) {
+            return null;
+        }
+
+        List<Map<String, Object>> lst = new ArrayList<>(maps.length);
+
+        for (Map<String, Object> map : maps) {
+            Map<String, Object> newMap = new LinkedHashMap<>();
+            for (Map.Entry<String, Object> entry : map.entrySet()) {
+                String key = entry.getKey();
+                key=key.trim();
+                if (ch2enMap.containsKey(key)) {
+                    key = ch2enMap.get(key);
+                }
+                newMap.put(key, entry.getValue());
+            }
+            lst.add(newMap);
+        }
+
+        return lst;
     }
 }
